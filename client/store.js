6c829c828968cf85460968ec027fa730de420ab5
Travis Gorman <thefarbetween@me.com>
2017-03-08 03:19:56 -0600

    set up redux store

--- client/store.js
+++ client/store.js
@@ -2,7 +2,6 @@ import { createStore, compose } from 'redux'
 import { syncHistoryWithStore } from 'react-router-redux'
 import { browserHistory } from 'react-router'
 
-
 // import the root reducer
 import rootReducer from './reducers/index'
 
@@ -11,18 +10,14 @@ import comments from './data/comments'
 import posts from './data/posts'
 
 //create an object for the default data
+const defaultState = {
+	posts,
+	comments,
+}
 
-// const defaultState = {
-// 	posts,
-// 	comments,
-// }
+const store = createStore(  
+  rootReducer, defaultState
+);
 
-// const store = createStore(  
-//   rootReducer, defaultState
-// );
-
-// export const history = syncHistoryWithStore(  
-//   browserHistory, store
-// );
-
-// export default store;
+export default const store = createStore(rootReducer, defaultState)
+export const history = syncHistoryWithStore(browserHistory, store)
