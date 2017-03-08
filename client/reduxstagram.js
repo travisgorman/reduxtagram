66a385c4c9b895cf2f04296501df16666f32eabf
Travis Gorman <thefarbetween@me.com>
2017-03-08 03:10:55 -0600

    app layout via Main component

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -8,14 +8,14 @@ import Main from './components/Main';
 import PhotoGrid from './components/PhotoGrid'
 import Single from './components/Single'
 
-const router = (  
-  <Router history={browserHistory}>
-  	<Route path="/" component={Main}>
-  		<IndexRoute component={PhotoGrid}></IndexRoute>
-  		<Route path="/view/:postId" component={Single}></Route>
-  	</Route>
-  </Router>
-)
+// const router = (  
+//   <Router history={browserHistory}>
+//   	<Route path="/" component={Main}>
+//   		<IndexRoute component={PhotoGrid}></IndexRoute>
+//   		<Route path="/view/:postId" component={Single}></Route>
+//   	</Route>
+//   </Router>
+// )
 
 render( 
 	router, 
--- client/store.js
+++ client/store.js
@@ -12,17 +12,17 @@ import posts from './data/posts'
 
 //create an object for the default data
 
-const defaultState = {
-	posts,
-	comments,
-}
+// const defaultState = {
+// 	posts,
+// 	comments,
+// }
 
-const store = createStore(  
-  rootReducer, defaultState
-);
+// const store = createStore(  
+//   rootReducer, defaultState
+// );
 
-export const history = syncHistoryWithStore(  
-  browserHistory, store
-);
+// export const history = syncHistoryWithStore(  
+//   browserHistory, store
+// );
 
-export default store;
+// export default store;
