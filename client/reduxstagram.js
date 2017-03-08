6cd892df9d212761868fca2a6025f74f60e04edc
Travis Gorman <thefarbetween@me.com>
2017-03-08 04:50:32 -0600

    integrate store with provider

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -8,13 +8,22 @@ import Main from './components/Main';
 import PhotoGrid from './components/PhotoGrid'
 import Single from './components/Single'
 
+import { Provider } from 'react-redux'
+import store, { history } from './store'
+
+
+// wrap the entire router in the Provider tag to expose the store
+// swap browserHistory with our own 'history' - imported from 'store'
+
 const router = (  
-  <Router history={browserHistory}>
-  	<Route path="/" component={Main}>
-  		<IndexRoute component={PhotoGrid}></IndexRoute>
-  		<Route path="/view/:postId" component={Single}></Route>
-  	</Route>
-  </Router>
+	<Provider store={store} >	
+	  <Router history={history}>
+	  	<Route path="/" component={Main}>
+	  		<IndexRoute component={PhotoGrid}></IndexRoute>
+	  		<Route path="/view/:postId" component={Single}></Route>
+	  	</Route>
+	  </Router>
+	</Provider>
 )
 
 render(router, document.getElementById('root'))
