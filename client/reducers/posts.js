0b46dd003916a962d5d85f92e34faf7c51ac3280
Travis Gorman <thefarbetween@me.com>
2017-03-10 07:32:38 -0600

    posts reducer increment likes

--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -1,6 +1,17 @@
-function posts(state = [], action) 	{
-	console.log( 'posts did an action', state, action )
-	return state;
+function posts(state = [], action) {
+
+	switch(action.type) {
+		case 'INCREMENT_LIKES' :
+		console.log( "incrementing Likes!!" );
+		const i = action.index;
+		return [
+			...state.slice(0,i),
+			{...state[i], likes: state[i].likes + 1 },
+			...state.slice(i + 1),
+		]
+		default:
+			return state;
+	}	
 }
 
 export default posts;
